11173be373512ac58a40b43c794063141ac4d2f9
BuildTools <luigimassa2005@gmail.com>
2023-09-26 23:18:03 +0200

    Aggiornamento

--- client.py
+++ client.py
@@ -1,29 +1,43 @@
 import sys
 import socket
+
+#Funzione che invia comandi o messaggi al server. Prende come parametro il socket
 def invia_comandi(s):
     print("Ecco un esempio di comandi da poter inviare.\n- ipconfig\n- ls\n- pwd")
+    #Aspettiamo sempre l'input del client
     while True:
         messaggio = input("Inserisci messaggio: ")
+        #Se il messaggio è == a lange, la connessione al server si chiuderà
         if messaggio == "lange":
             print("Chiusura della connessione")
+            #Chiusura socket
             s.close()
+            #Chiusura applicazione
             sys.exit()
         else:
+            #attraverso il send inviamo il messaggio che pero' deve essere trasmesso in bytes
             s.send(messaggio.encode())
+            #Qui riceviamo una risposta dal server in un buffer da 4096 bytes
             data = s.recv(4096)
+            #Stampiamo a schermo decodificando il messaggio in bytes ricevuto
             print(str(data, "utf-8"))
 
+#Funzione che crea la connessione al server che prende come parametro un indirizzo
 def connessione(indirizzo):
     try:
+        #Creazione socket
         s = socket.socket()
+        #proviamo a connetterci
         s.connect(indirizzo)
         print(f"Connessione al server {indirizzo} stabilita")
     except socket.error as errore:
         print(f"Errore, non riesco a continuare. \nTipo errore: {errore}")
         sys.exit()
+    #Se tutto è andato a buon fine possiamo inviare comandi
     invia_comandi(s)
 
 if __name__ == "__main__":
+    #Porta a nostro piacimento
     connessione(("127.0.0.1", 15000))
 
 
--- server.py
+++ server.py
@@ -1,12 +1,14 @@
 import socket
 import subprocess
 
-
+#Funzione che risponde in base al messaggio inviato, prende in input la connessione con il client
 def ricevi_comando(connessione):
 	while True:
+		#Riceviamo una richiesta
 		richiesta = connessione.recv(4096)
 		print(f"L'utente ha inviato: {richiesta.decode()}")
 
+		#Messaggi predefiniti di risposta
 		if richiesta.decode().lower() == "ciao":
 			messaggio = "Ciao user, come stai?".encode()
 			connessione.send(messaggio)
@@ -14,25 +16,34 @@ def ricevi_comando(connessione):
 			messaggio = "Progetto fatto da Vasil, Lange, Luigi, Christian".encode()
 			connessione.send(messaggio)
 		elif richiesta.decode().lower() == "link":
-			messaggio = "Link powerpoint: https\nLink codice: https".encode()
+			messaggio = "Link powerpoint: https://www.canva.com/design/DAFvdgu15F4/ury6Jyhj3oGIKI6_mKcytA/view?utm_content=DAFvdgu15F4&utm_campaign=designshare&utm_medium=link&utm_source=editor\nLink codice: https://github.com/GGNado/server_client".encode()
 			connessione.send(messaggio)
 		else:
+			#Entriamo qui se il messaggio predefinito non esiste, probabilmente perchè l'utente ha inviato un comando come ls
 			risposta = subprocess.run(richiesta.decode(), shell = True, stdout = subprocess.PIPE, stderr = subprocess.PIPE)
 			data = risposta.stdout + risposta.stderr
+			#Se il comando non esiste risponderà con [comando]: command not found
 			connessione.send(data)
 
 def server(indirizzo, backlog=1):
 	try:
 		s = socket.socket()
+		#con bind diciamo al socket di fare da server passandogli l'indirizzo dove i client manderanno richieste
 		s.bind(indirizzo)
+		#Con backlog diciamo quante connessioni in attesa posso esserci
 		s.listen(backlog)
 		print("Server pronto e in ascolto")
 	except socket.error as errore:
 		print(f"Errore. \nTipo errore {errore}.\nProvo a ristartare")
+		#Richiamiamo la funzione in caso ci sia stato qualche errore di avvio
 		server(indirizzo)
+
+	#Il client si sta connettendo
 	connessione, indirizzo_del_client = s.accept()
 	print(f"Connessione stabilita con {indirizzo_del_client} ")
+	#Pronto a ricevere
 	ricevi_comando(connessione)
 
 if __name__ == "__main__":
+	#Porta a nostro piacimento
 	server(("127.0.0.1", 15000))
